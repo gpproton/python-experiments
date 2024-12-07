0cd30142a3611d1caf59f94cb92fc1897b95b1a3
Godwin peter .O <me@godwin.dev>
2024-12-07 11:21:04 +0100

    added readme doc and draft for background task

---
+++ README.md
@@ -0,0 +1,8 @@
+# Python Experiments
+
+This repository contains python code i find intresting/experiment with in my free time for later refrence or possible learning while taking a breather from any chalenging project.
+
+### Samples
+- [Chunked async excustion](./chunked_async.py)
+- [Trip route distance and ETA resolver](./trip_resolver.py)
+- [Long running background tasks - Draft](./background_tasks.py)
---
+++ background_tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+Copyright (c) 2024 Godwin Peter .O
+
+Licensed under the MIT License
+you may not use this file except in compliance with the License.
+    https://opensource.org/license/mit
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Godwin peter .O (me@godwin.dev)
+Created At: Saturday, 7th Dec 2024
+Modified By: Godwin peter .O
+Modified At: Sat Dec 07 2024
+'''
+
